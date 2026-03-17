3609979ed8deced0f5ebea9470cf763ba2952a7f
expexpert <upworkwebexpert@gmail.com>
2026-03-17 15:04:31 +0530

    i18n configuration and json added

---
+++ src/i18n/i18n.ts
@@ -0,0 +1,53 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import * as RNLocalize from 'react-native-localize'
+
+import en from './locales/en.json'
+import fr from './locales/fr.json'
+
+const resources = {
+  en: { translation: en },
+  fr: { translation: fr }
+}
+
+const LANGUAGE_KEY = 'appLanguage'
+
+const languageDetector = {
+  type: 'languageDetector',
+  async: true,
+
+  detect: async (callback: any) => {
+    const savedLanguage = await AsyncStorage.getItem(LANGUAGE_KEY)
+
+    if (savedLanguage) {
+      callback(savedLanguage)
+      return
+    }
+
+    const locales = RNLocalize.getLocales()
+    const deviceLanguage = locales?.[0]?.languageCode ?? 'en'
+
+    callback(deviceLanguage)
+  },
+
+  init: () => {},
+
+  cacheUserLanguage: async (language: string) => {
+    await AsyncStorage.setItem(LANGUAGE_KEY, language)
+  }
+}
+
+i18n
+  .use(languageDetector as any)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    },
+    initImmediate: false,
+  })
+
+export default i18n
