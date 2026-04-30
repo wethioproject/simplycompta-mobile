3bd9f7848601cf3d21616f6f24c5d8e6a42e105d
expexpert <upworkwebexpert@gmail.com>
2026-04-30 17:26:48 +0530

    feat(invoice): changed file size limit 2MB on frontend

--- src/components/invoice/CreateInvoiceModal.tsx
+++ src/components/invoice/CreateInvoiceModal.tsx
@@ -125,7 +125,7 @@ const CreateInvoiceModal: React.FC<{
   const [removedExistingDocument, setRemovedExistingDocument] = useState(false);
   const [fileSizeError, setFileSizeError] = useState(false);
 
-  const MAX_FILE_SIZE = 1024 * 1024; // 1024 KB
+  const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2 MB
 
   const [localClients, setLocalClients] = useState<Client[]>(clients);
   React.useEffect(() => { setLocalClients(clients); }, [clients]);
